e6ce6ae982e05053fe1263007858a80b4bb65fc9
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-03-07 07:06:29 -0600

    Initialize tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff4800',
+      }
+    },
+  },
+  plugins: [],
+};
