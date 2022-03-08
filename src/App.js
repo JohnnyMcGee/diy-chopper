23fbc2dad6cf59e17799cdedea9d602cd356c61e
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-03-08 06:24:40 -0600

    Install Material UI and at Navigation appbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-      <div>
-        <div className="text-xl font-medium text-primary">ChitChat</div>
-        <p className="text-slate-500">You have a new message!</p>
-      </div>
+    <div>
+      <Navigation></Navigation>
     </div>
-  );
+  );                
 }
 
 export default App;
