7d8b72bc7ec3a6472a7fa92cfc1c47782f8d17ae
Johnny <johnnymcgee.dev@protonmail.com>
2022-03-11 12:17:08 -0600

    Create project view page, add basic structure to detail area

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navigation from "./components/Navigation";
+import ProjectView from "./pages/ProjectView";
 
 function App() {
   return (
     <div>
       <Navigation></Navigation>
+      <ProjectView/>
     </div>
   );                
 }
---
+++ src/pages/ProjectView.js
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Box,
+  Grid,
+  Typography,
+  Paper,
+  Button,
+  Avatar,
+  Card,
+  CardHeader,
+  Stack,
+} from "@mui/material";
+
+const ProjectView = () => {
+  return (
+    <Box>
+      <Typography variant="h6" gutterBottom>
+        Project Title Goes Here
+      </Typography>
+      <Box sx={{ height: "240px", backgroundColor: "gray" }}></Box>
+      <Paper elevation={3}>
+        <Grid container spacing={2} sx={{ px: 3, py: 1 }}>
+          <Grid item xs={6}>
+            <Card elevation={0}>
+              <CardHeader
+                avatar={<Avatar />}
+                title="Account Name"
+                subheader="account status"
+              />
+            </Card>
+          </Grid>
+          <Grid item xs={3} sx={{ m: "auto 0" }}>
+            <Typography
+              sx={{ verticalAlign: "center" }}
+              variant="body2"
+              align="center"
+            >
+              Started: March 6, 2022
+            </Typography>
+          </Grid>
+          <Grid item xs={3} sx={{ m: "auto 0" }}>
+            <Typography variant="body2" align="center">
+              Started: March 6, 2022
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body1">
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </Typography>
+          </Grid>
+          <Grid item xs={12} align="center">
+            <Button variant="contained">Save Inspiration</Button>
+          </Grid>
+          secondary
+        </Grid>
+      </Paper>
+      <Stack></Stack>
+    </Box>
+  );
+};
+
+export default ProjectView;
